c17e5072ae08ea3cb236fe7da0939ee2a173d8e6
Felipe Virasoro <felipe.virasoro.contractor@bbva.com>
2025-02-10 21:00:50 +0100

    fix the endpoint

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,19 +1,17 @@
 export async function fetchOrderData() {
     try {
-        const isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
-        const response = isLocal 
-            ? await fetch('./mock/mock.json')  // Datos locales desde el mock
-            : await fetch('https://technical-test.webmefy.io/webmefy/data', {
-                method: 'GET',
-                headers: {
-                    'X-Webmefy-Token': 'Webmefysupersecretentryleveltoken',
-                    'Accept': 'application/json',
-                }
-            });
+        const response = await fetch('https://technical-test.webmefy.io/webmefy/data', {
+            method: 'GET',
+            headers: {
+                'X-Webmefy-Token': 'Webmefysupersecretentryleveltoken',
+                'Accept': 'application/json',
+            }
+        });
 
         if (!response.ok) {
             throw new Error(`Error: ${response.statusText}`);
         }
+
         return await response.json();
     } catch (error) {
         console.error('Error fetching order data:', error);
