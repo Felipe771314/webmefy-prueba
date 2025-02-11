e5f7348aa4d3b45e1d30bd9213c188ecf2e9d4de
Felipe Virasoro <felipe.virasoro.contractor@bbva.com>
2025-02-11 21:02:39 +0100

    fix the project forward to 22

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,23 @@
 export default async function handler(req, res) {
     const apiUrl = 'https://technical-test.webmefy.io/webmefy/data';
-    
-    const response = await fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            'X-Webmefy-Token': 'Webmefysupersecretentryleveltoken',
-            'Accept': 'application/json',
-        },
-    });
 
-    if (!response.ok) {
-        return res.status(response.status).json({ error: 'Error fetching data' });
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'X-Webmefy-Token': 'Webmefysupersecretentryleveltoken',
+                'Accept': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            return res.status(response.status).json({ error: `Failed to fetch: ${response.statusText}` });
+        }
+
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error en el proxy:', error);
+        res.status(500).json({ error: 'Error interno del servidor', details: error.message });
     }
-
-    const data = await response.json();
-    res.status(200).json(data);
 }
