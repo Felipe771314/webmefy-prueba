0de5dc064d2b9050ee2974b1f48bdb9e7dbef6d1
Felipe Virasoro <felipe.virasoro.contractor@bbva.com>
2025-02-11 18:53:53 +0100

    fix the project 11

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,13 +1,16 @@
 export async function fetchOrderData() {
-  try {
-      const response = await fetch('/api/proxy');
-
+    try {
+      console.log("Intentando obtener datos desde /api/proxy...");
+      const response = await fetch("/api/proxy");
+  
       if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
+        throw new Error(`Error: ${response.statusText}`);
       }
-
+  
+      console.log("Respuesta obtenida:", response);
       return await response.json();
-  } catch (error) {
-      console.error('Error fetching order data:', error);
+    } catch (error) {
+      console.error("Error fetching order data:", error);
+    }
   }
-}
+  
